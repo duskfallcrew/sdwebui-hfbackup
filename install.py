ee32c02adaf20f3459e0b12804463869e13ea20d
The Duskfall Portal Crew <58930427+duskfallcrew@users.noreply.github.com>
2024-12-26 18:15:39 +1300

    Update and rename install.py to __init__.py

--- install.py
+++
@@ -1,9 +0,0 @@
-import launch
-
-if not launch.is_installed("huggingface_hub"):
-    launch.run_pip("install huggingface_hub==4.30.2", "requirements for Hugging Face Hub Uploader")
-
-if not launch.is_installed("glob2"):
-    launch.run_pip("install glob2", "requirements for Hugging Face Hub Uploader")
-
-launch.run_pip("install -r requirements.txt")
