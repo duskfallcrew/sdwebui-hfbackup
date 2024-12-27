81340568cc76539eed3985c3003804f5dd4f42f9
John6666 <186692226+John6666cat@users.noreply.github.com>
2024-12-27 10:01:02 +0900

    Update hfbackup_script.py

--- scripts/hfbackup_script.py
+++ scripts/hfbackup_script.py
@@ -240,7 +240,9 @@ class HFBackupScript():
         return on_ui(self)
     
     def update_schedule(self):
-        if "backup" in self.scheduler.get_jobs(): self.scheduler.remove_job("backup")
+        if "backup" in self.scheduler.get_jobs():
+            self.scheduler.shutdown(wait=False)
+            self.scheduler.remove_job("backup")
         self.scheduler.add_job(func=backup_files, args=[self.backup_paths, self], trigger="interval", id="backup", seconds=BACKUP_INTERVAL)
         self.scheduler.start()
 
